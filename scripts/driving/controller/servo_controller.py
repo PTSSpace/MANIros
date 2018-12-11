eb62a67b54270aeac9441424218a6afc41949653
Leon Knauer <leon.knauer@ptscientists.com>
2018-12-11 21:25:26 +0100

    Using the param server for servo config values

--- scripts/driving/controller/servo_controller.py
+++ scripts/driving/controller/servo_controller.py
@@ -7,17 +7,8 @@ import time
 # Import the PCA9685 module.
 import Adafruit_PCA9685
 
-
-# Uncomment to enable debug output.
-#import logging
-#logging.basicConfig(level=logging.DEBUG)
-
 # Initialise the PCA9685 using the default address (0x40).
 pwm = Adafruit_PCA9685.PCA9685(address=0x40, busnum=1)
-
-# Configure min and max servo pulse lengths
-servo_min = 1024  # Min pulse length out of 4096
-servo_max = 3072  # Max pulse length out of 4096
 
 # Set frequency to 60hz, good for servos.
 pwm.set_pwm_freq(60)
@@ -25,11 +16,11 @@ pwm.set_pwm_freq(60)
 def callback(data):
     rospy.loginfo("Servo controller: I've heard fr:%d fl:%d rl:%d rr:%d. Moving Servos!" % (data.front_right_angle, data.front_left_angle, data.rear_left_angle, data.rear_right_angle));
     
-    pwm.set_pwm(0, 0, servo_min)
-    time.sleep(1)
+    pwm.set_pwm(1, 0, rospy.get_param("/servo_controller/servo1_max"))
+    time.sleep(0.1)
 
-    pwm.set_pwm(0, 0, servo_max)
-    time.sleep(1)
+    pwm.set_pwm(1, 0, rospy.get_param("/servo_controller/servo1_max"))
+    time.sleep(0.1)
 
 def listener():
     rospy.init_node("servo_controller", anonymous=True);
@@ -38,5 +29,3 @@ def listener():
 
 if __name__ == '__main__':
     listener();
-
-
