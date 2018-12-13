6b43a2ec05ee793498139c5e3612d94cba297d96
Leon Knauer <leon.knauer@ptscientists.com>
2018-12-13 19:09:09 +0100

    Adding creation of src folder to run.sh

--- ci/run.sh
+++ ci/run.sh
@@ -4,11 +4,13 @@
 apt-get update
 apt-get install -y i2c-tools libi2c-dev
 
-# create workspace and load environment
-mkdir ~/catkin_ws
+# source environment
+source /opt/ros/kinetic/setup.bash
+
+# create workspace
+mkdir -p ~/catkin_ws/src
 ln -s /source ~/catkin_ws/src
 cd ~/catkin_ws
-source /opt/ros/kinetic/setup.bash
 
 # build package
 catkin_make
