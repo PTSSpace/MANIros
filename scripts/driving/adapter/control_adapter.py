e47faa7bd995cbd0ddc936d4bcf8c449b1cced3e
heyegross <heye.gross@ptscientists.com>
2018-12-18 16:42:10 +0100

    added speed normalization and preliminary backwards servo fix

--- scripts/driving/adapter/control_adapter.py
+++ scripts/driving/adapter/control_adapter.py
@@ -10,8 +10,8 @@ def callback(data):
     rospy.loginfo("Adapter: I've heard x:%d y:%d rot:%d - translating..." % (data.xDistance, data.yDistance, data.rotationDistance));
     msg = translateRoverControl(data);
     pub.publish(msg);
-    rospy.loginfo("Adapter: I've sent angles fl:%d rl:%d rr:%d fr:%d" % (msg.front_left_angle, msg.rear_left_angle, msg.rear_right_angle, msg.front_right_angle));
-    rospy.loginfo("Adapter: and velocities fl:%d rl:%d rr:%d fr:%d" % (msg.front_left_speed, msg.rear_left_speed, msg.rear_right_speed, msg.front_right_speed));
+    rospy.loginfo("Adapter: I've sent angles fl:%.3f rl:%.3f rr:%.3f fr:%.3f" % (msg.front_left_angle, msg.rear_left_angle, msg.rear_right_angle, msg.front_right_angle));
+    rospy.loginfo("Adapter: and velocities fl:%.3f rl:%.3f rr:%.3f fr:%.3f" % (msg.front_left_speed, msg.rear_left_speed, msg.rear_right_speed, msg.front_right_speed));
 
 def listener():
     rospy.init_node("control_adapter", anonymous=True);
@@ -23,21 +23,23 @@ def translateRoverControl(data):
 
     #MISSING
     #import some config file containing the wheel-center distance between 
-    #the front and back wheels (height) and 
-    #the left and right wheels (width)
+    #the front and back wheels (height) and (in regards to front: camera head position)
+    #the left and right wheels (width) 
     
     robot_height = 10
     robot_width = 5
 
     #rotation, the scaled rotationDistance is multiplied with the width or height, returning a rotation component
-    #this rotationHeightFactor = r_fac_x (as height is along the x axis)
-    #amd rotationWidthFactor = r_fac_y
-    r_fac_x = (robot_height * data.rotationDistance) / math.hypot(robot_width, robot_height)
-    r_fac_y = (robot_width * data.rotationDistance) / math.hypot(robot_width, robot_height)
-
+    #this rotationWidthFactor = r_fac_x (as height is along the x axis)
+    #amd rotationHeightFactor = r_fac_y
+    r_fac_x = (robot_width * data.rotationDistance) / math.hypot(robot_width, robot_height)
+    r_fac_y = (robot_height * data.rotationDistance) / math.hypot(robot_width, robot_height)
+    
     wheelIndexArray = [1, 2, 3, 4] #see Motor Layout in KB for index, 1 = front-left then clockwise
     wheelAngleArray = []
     wheelSpeedArray = []
+    
+    # rotationFactor  + translation (the vector xDistance, yDistance) = finalVector
     for wheel in wheelIndexArray:
         
         if 2 <= wheel <= 3: #assigns a positiv X Rotation to all rear wheels
@@ -49,23 +51,39 @@ def translateRoverControl(data):
         else:
             calcY = r_fac_y + data.yDistance
         
-        wheelAngleArray.append(calcY) #math.atan2(calcY, calcX))
-        wheelSpeedArray.append(math.hypot(calcX, calcY))
         
+        angleXY = math.atan2(calcY, calcX)
+        speedXY = math.hypot(calcX, calcY)
+        #MISSING -- could also be implementet on the servo side
+        #reverse speed if necessary, need Implementation of WHEN an angle should be turned
+        # this has to be determined by the hardware orientation of the servo
+        if math.fabs(angleXY) > (math.pi / 2): #this > (math.pi / 2) should be changed
+            angleXY -= math.copysign(math.pi, angleXY)
+            speedXY *= -1
         
-    #translation (the vector xDistance, yDistance) + rotationFactor = finalVector
+        wheelAngleArray.append(angleXY)
+        wheelSpeedArray.append(speedXY)
+        
+    
+    #normalize and 
 
-
+    maxspeed = max(wheelSpeedArray)
+    if maxspeed > 1:
+        for index, speed in enumerate(wheelSpeedArray):
+            wheelSpeedArray[index] = speed / maxspeed
+    
+    
+    
    
-    msg.front_left_angle = wheelAngleArray[0] * 1000
-    msg.rear_left_angle = wheelAngleArray[1] * 1000
-    msg.rear_right_angle = wheelAngleArray[2] * 1000
-    msg.front_right_angle = wheelAngleArray[3] * 1000
+    msg.front_left_angle = wheelAngleArray[0]
+    msg.rear_left_angle = wheelAngleArray[1]
+    msg.rear_right_angle = wheelAngleArray[2]
+    msg.front_right_angle = wheelAngleArray[3]
 
-    msg.front_left_speed = wheelSpeedArray[0] * 1000
-    msg.rear_left_speed = wheelSpeedArray[1] * 1000
-    msg.rear_right_speed = wheelSpeedArray[2] * 1000
-    msg.front_right_speed = wheelSpeedArray[3] * 1000
+    msg.front_left_speed = wheelSpeedArray[0]
+    msg.rear_left_speed = wheelSpeedArray[1]
+    msg.rear_right_speed = wheelSpeedArray[2]
+    msg.front_right_speed = wheelSpeedArray[3]
 
     return msg;
 
