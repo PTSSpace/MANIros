95f4fc89b509f3c69b95121076cdd26a22205196
mawalu <web@mawalabs.de>
2019-01-04 16:22:52 +0100

    Start implementing integration tests for adapter

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -2,6 +2,9 @@
 
 import rostest
 from example import TestBareBones
+from control_adapter_test import ControlAdapterTest
 
 # rostest
 rostest.rosrun('maniros', 'test_bare_bones', TestBareBones)
+rostest.rosrun('maniros', 'test_basic_node_actions', ControlAdapterTest)
+
