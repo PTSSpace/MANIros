3d35080ae127bb124bd12141ffa6c1eb43c6b476
max-nussbaum <privat.maxnussbaum@gmail.com>
2019-03-26 14:42:12 +0100

    include travis build ros dependencies

--- ci/run.sh
+++ ci/run.sh
@@ -7,6 +7,10 @@ apt-get install -y i2c-tools libi2c-dev
 # source environment
 source /opt/ros/kinetic/setup.bash
 
+# install ros dependencies
+apt-get install -y ros-kinetic-geometry
+apt-get install -y ros-kinetic-geometry2
+
 # create workspace
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws
