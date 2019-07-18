8c8d37983d668daf47ce87e4a5892f4bf1aa4256
MircoPTS <mirco.richter@ptscientists.com>
2019-07-18 16:54:41 +0200

    enoder_simulation implemented

--- scripts/can/encoder_simulation.py
+++ scripts/can/encoder_simulation.py
@@ -17,32 +17,61 @@ Imports
 """
 import math
 import rospy
+from maniros.msg import EncoderOdometry
+from sensor_msgs.msg import JointState
 
 """
 Global variables
 """
-PULSES_PER_REV          8384
+PULSES_PER_REV          = 8384
 
-MAX_VEL                 = 3                                            			# Maximal wheel velocity [rad/s]
+MAX_VEL                 = math.pi                                            	# Maximal wheel velocity [rad/s]
 MaxOrientation          = math.pi                          						# Maximal wheel orientation [rad]
 MaxVelEnc 				= MAX_VEL * PULSES_PER_REV/(2*MaxOrientation)   		# Maximal wheel velocity [encoder pulses per second]
 MaxOrEnc				= PULSES_PER_REV/2                            			# Maximal wheel orientation [encoder pulses]
 
 PUB_RATE 				= 5														# Rate to publish odometry data [Hz]
 
+Angle_Old 				= 0														# last knows wheelAngle
+Rotations 				= 0														# counter for full rotations scince start
+
 """
 Classes
 """
 
 class EncoderSimulation():
-	def __init__(self, name):
-        # Joint velocity and orientation subscriber
-        self.joint_sub = rospy.Subscriber("/mani/joint_states", JointState, self.get_joint_states, queue_size=10)
+	def __init__(self):
+		self.wheelSpeed         = [0, 0, 0, 0]		#rad/s
+		self.wheelAngle         = [0, 0, 0, 0]		#rad
+		# Joint velocity and orientation subscriber
+		self.joint_sub = rospy.Subscriber("/mani/joint_states", JointState, self.get_joint_states, queue_size=10)
         # Odometry publisher base_link
-        self.enc_pub = rospy.Publisher("encoder_odometry", ???, queue_size=10)
+		self.enc_pub = rospy.Publisher("encoder_odometry", EncoderOdometry, queue_size=10)
 
-    def shutdown(self):
-        rospy.loginfo("Shutting down odometry node")
+	def get_joint_states(self, data):
+		# Get joint state values from simulation
+		self.wheelAngle = data.position[6:10]
+		self.wheelSpeed = data.velocity[2:6]
+
+	def shutdown(self):
+		rospy.loginfo("Shutting down odometry node")
+
+	def rad_to_pulse(vel):
+		return vel/(2*math.pi)*PULSES_PER_REV
+
+	def revolutions_counter(wheelAngle,wheelSpeed):
+		if(wheelAngle-Angle_Old>1.5*math.pi and wheelSpeed>0):
+			Rotations=Rotations+1
+			Angle_Old=wheelAngle
+
+		if(Angle_Old-wheelAngle>1.5*math.pi and wheelSpeed<0):
+			Rotations=Rotations-1
+			Angle_Old=wheelAngle
+    	
+		return Rotations
+
+
+
 
 """
 Main
@@ -52,7 +81,7 @@ if __name__ == '__main__':
     rospy.init_node("odometry_simulation", anonymous=True)
 
     # Start ROS action
-    server = LocomotionSimulation(rospy.get_name())
+    encoder = EncoderSimulation()
     rospy.spin()
 
 rospy.on_shutdown(server.shutdown)
