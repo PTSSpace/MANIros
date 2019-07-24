311c62fea203356c7e8a80ed4caf1d0fe71770cb
max-nussbaum <privat.maxnussbaum@gmail.com>
2019-07-24 15:05:37 +0200

    Revised ROS tests

--- tests/cmd_vel/cmd_vel_test.py
+++ tests/cmd_vel/cmd_vel_test.py
@@ -15,17 +15,22 @@ class CmdVelTest(unittest.TestCase):
         rospy.init_node("test_input", anonymous=True)
         self.pub_move = rospy.Publisher("move_base/cmd_vel", Twist, queue_size=10)
         self.pub_teleop = rospy.Publisher("teleop/cmd_vel", Twist, queue_size=10)
-        rospy.Subscriber("cmd_vel", MoveControl, self.callback)
+        # Locomotion control action
+        self._action_name = "locomotion_control"
+        self._as = actionlib.SimpleActionServer(self._action_name, LocomotionAction, execute_cb=self.action_cb, auto_start = False)
+        self._as.start()
         self.success = False
 
-    def callback(self, data):
+    def action_cb(self, goal):
         rospy.loginfo("Linear: I've heard x:%f y:%f." % (
-            data.x,
-            data.y
+            goal.command.x,
+            goal.command.y
             ))
         rospy.loginfo("Rotation: I've heard rz:%f." % (
-            data.rz
+            goal.command.rz
             ))
+        self._result.sequence = [1]
+        self._as.set_succeeded(self._result)
         self.success = True
 
     def test_teleop_actions(self):
@@ -38,13 +43,14 @@ class CmdVelTest(unittest.TestCase):
         msg.angular.z = 0.1
 
         self.pub_teleop.publish(msg)
-        timeout_t = time.time() + 10.0 #10 seconds
 
+        timeout_t = time.time() + 10.0  # 10 s
+        rate = rospy.Rate(10)           # 10 Hz
         while not rospy.is_shutdown() and not self.success and time.time() < timeout_t:
             self.pub_teleop.publish(msg)
-            time.sleep(0.1)
+            rate.sleep()
 
-        self.assertTrue(self.success, 'Callback has not been called')
+        self.assertTrue(self.success, 'Locomotion action has not been called')
 
     def test_autonomous_actions(self):
 
@@ -55,22 +61,23 @@ class CmdVelTest(unittest.TestCase):
         msg.linear.y = 0.0
         msg.angular.z = -1.0
 
-        timeout_t = time.time() + 10.0 #10 seconds
-
         # Test blocking by human command
         self.pub_teleop.publish(msg)
+
+        timeout_t = time.time() + 10.0  # 10 s
+        rate = rospy.Rate(10)           # 10 Hz
         while not rospy.is_shutdown() and not self.success and time.time() < timeout_t:
             self.pub_teleop.publish(msg)
-            time.sleep(0.1)
+            rate.sleep()
 
         self.success = False
 
         self.pub_move.publish(msg)
         while not rospy.is_shutdown() and not self.success and time.time() < timeout_t:
             self.pub_move.publish(msg)
-            time.sleep(0.1)
+            rate.sleep()
 
-        self.assertTrue(self.success, 'Callback has not been called')
+        self.assertTrue(self.success, 'Locomotion action has not been called')
 
 if __name__ == '__main__':
     rostest.rosrun('maniros', 'test_cmd_vel', CmdVelTest)
