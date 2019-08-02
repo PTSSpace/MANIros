ef57290a23755c0ba2d0deb3431e8ed48116bbae
max-nussbaum <privat.maxnussbaum@gmail.com>
2019-08-02 12:45:24 +0200

    ..

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['driving'],
-    packages=['feeling'],
+    packages=['driving', 'feeling'],
     package_dir={'': 'scripts'},
 )
 
